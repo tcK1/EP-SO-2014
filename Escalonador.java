457bf14bf6cc20ac6b32818e73844c6873052d9d
unknown <William@Nostradamus.lambda3.com.br>
2014-10-29 17:51:56 -0200

    Agora escreve no arquivo log.txt cada processo que eh carregado

--- Escalonador.java
+++ Escalonador.java
@@ -5,19 +5,35 @@ public class Escalonador{
 
 	static Processo tabela[] = new Processo[10]; /* tabela de procesos */
 	static int quantum;
-	static Processo executando = 1;
+	static Processo executando = null;
 	static Fila pronto = new Fila();
 	static Fila bloqueado = new Fila();
 	
 	public static void main(String[] args){
 		try{
-			criaProcessos();
+			
+			String diretorio = System.getProperty("user.dir");
+			int mediaTrocas = 0;
+			int mediaInstrucoes = 0;
+			
 			leQuantum();
-			while(!estaVazia()){ /* enquanto houver processos na tabela de processos */
+					
+			File log = new File( diretorio + "/log"+quantum+".txt" );/* instancia novo documento de texto para o log */
+			if(!log.exists()) log.createNewFile(); /* caso este nao exista, um novo e criado */
+			
+			criaProcessos(log);
+			
+			FileWriter olog = new FileWriter(log.getPath(), true);
+			BufferedWriter wlog = new BufferedWriter(olog);
+			PrintWriter pw = new PrintWriter(wlog);
+			
+			while(!tabelaDeProcessosEstaVazia()) /* enquanto houver processos na tabela de processos */
+			{ 
 				decrementaBloqueados(); /* decrementa o contador da fila dos bloqueados */
 				executando = pronto.pop(); /* obtem o proximo a executar */
 				executando.status = "Executando";
-				if(executando == null){
+				if(executando == null)
+				{
 					continue;
 				}
 				boolean paraPronto = true;
@@ -25,48 +41,90 @@ public class Escalonador{
 					String instrucao = executando.DS[executando.PC];
 					executando.PC++;
 					switch(instrucao){
-							case "COM": 
+						case "COM": 
 							if(quantum - c == 1){ /* depois da ultima iteracao, inserir na fila de prontos */
 								executando.status = "Pronto";
-								pronto.push(executando);	
+								pronto.push(executando);
+								pw.println("Interrompendo "+executando.nome+" apos "+quantum+" operacoes");
+//								wlog.write("Interrompendo "+executando.nome+" apos "+quantum+" operacoes");
+//								wlog.newLine();
+								break;
 							}
+							pw.println("Executando "+ executando.nome);
+//							wlog.write("Executando "+ executando.nome);
+//							wlog.newLine();
 							break;
 						case "E/S":
 							executando.status = "Bloqueado";
 							bloqueado.push(executando);
 							c = quantum;
+							if(c == 0){
+								pw.println("Interrompendo "+executando.nome+" apos 0 instrucao (havia apenas a E/S)");
+//								wlog.write("Interrompendo "+executando.nome+" apos 0 instrucao (havia apenas a E/S)");
+//								wlog.newLine();
+							}
+							if(c == 1){
+								pw.println("Interrompendo "+executando.nome+" apos 1 instrucao (havia um comando antes da E/S)");
+//								wlog.write("Interrompendo "+executando.nome+" apos 1 instrucao (havia um comando antes da E/S)");
+//								wlog.newLine();
+							}
+							if(c >= 2){
+								pw.println("Interrompendo "+executando.nome+" apos "+c+" instrucoes (haviam "+c+" comandos antes da E/S)");
+//								wlog.newLine();
+							}
 							break;
 						case "SAIDA":
 							finalizaProcesso(executando); /* remove processo da fila de bloqueados */
 							c = quantum;
+							pw.println(executando.nome+" terminado. X="+executando.X+" Y="+executando.Y);
+//							wlog.newLine();
 							break;
 						default:
-							switch(instrucao.substring(1,1)){
+							switch(instrucao.substring(0,1)){
 								case "X":
-									executando.X = Integer.parseInt(instrucao.substring(3));
+									executando.X = Integer.parseInt(instrucao.substring(2));
 									if(quantum - c == 1){ /* depois da ultima iteracao, inserir na fila de prontos */
 										executando.status = "Pronto";
 										pronto.push(executando);	
 									}
+									pw.println("Executando " + executando.nome);
 									break;
 								case "Y":
-									executando.Y = Integer.parseInt(instrucao.substring(3));
+									executando.Y = Integer.parseInt(instrucao.substring(2));
 									if(quantum - c == 1){ /* depois da ultima iteracao, inserir na fila de prontos */
 										executando.status = "Pronto";
 										pronto.push(executando);	
 									}
+									pw.println("Executando " + executando.nome);
+//									wlog.newLine();
 									break;
 							}
+						mediaInstrucoes++;
 					}
+					mediaTrocas++;
 				}
 			}
+
+			pw.println("MEDIA DE TROCAS: "+(mediaTrocas/10));
+			pw.println("MEDIA DE INSTRUCOES: "+(mediaInstrucoes/10));
+			pw.println("QUANTUM: "+quantum);
+			pw.close();
+			wlog.close();
+			olog.close();
+			
+//			wlog.write("MEDIA DE TROCAS: "+(mediaTrocas/10));
+//			wlog.newLine();
+//			wlog.write("MEDIA DE INSTRUCOES: "+(mediaInstrucoes/10));
+//			wlog.newLine();
+//			wlog.write("QUANTUM: "+quantum);						
+//			wlog.newLine();
 		}
 		catch(IOException ioe){
 			System.out.println(ioe.getMessage());
 		}
 	}
 	
-	static void criaProcessos() throws IOException{
+	static void criaProcessos(File log) throws IOException{
 		FileReader fr;
 		Processo p;
 		for(int i = 1; i <= 10; i++){
@@ -75,6 +133,14 @@ public class Escalonador{
 			p = new Processo(fr); 
 			tabela[i - 1] = p;
 			pronto.push(p);
+			
+			FileWriter olog = new FileWriter(log.getPath(), true);
+			BufferedWriter wlog = new BufferedWriter(olog);
+			PrintWriter pw = new PrintWriter(wlog);
+			pw.println("Carregando "+p.nome);
+			pw.close();
+			wlog.close();
+			olog.close();
 		}
 	}
 	
@@ -85,7 +151,7 @@ public class Escalonador{
 	}
 	
 	/* verifica se a tabela de processos esta vazia */
-	static boolean estaVazia(){
+	static boolean tabelaDeProcessosEstaVazia(){
 		for(int i = 0; i < 10; i++)
 			if(tabela[i] != null) return false;
 		return true;
@@ -93,7 +159,8 @@ public class Escalonador{
 	
 	/* decrementa o contador da fila de bloqueados
 	se algum processo tiver, ao final do procedimento, cont == 0, ele eh adicionado a fila de prontos */
-	static void decrementaBloqueados(){
+	static void decrementaBloqueados()
+	{
 		for(No n = pronto.inicio; n != null; n = n.prox) n.indice--;
 		No n = pronto.inicio;
 		while(n.indice == 0){ /* os mais antigos da fila de bloqueados esta sempre no inicio*/
